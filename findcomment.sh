aff1203c1fcbc23f957df8e87cb7109d17db4c28
mingregister <774262573@qq.com>
2020-05-31 23:21:04 +0800

    findcomment.sh add new keywork

--- findcomment.sh
+++ findcomment.sh
@@ -4,5 +4,7 @@ KEYWORD='mingregister'
 # KEYWORD='mingregister-配置服务路由'
 # KEYWORD='mingregister-InteractiveWithEtcd'
 # KEYWORD='mingregister-AccessControl'
+# KEYWORD='mingregister-iptables'
+# KEYWORD='mingregister-options'
 
 find ./ -type f -iname *.go | xargs fgrep "${KEYWORD}"
